327f87526bf5108b2c77c4722a5895a69eaed440
ismailfhg <ismailbayramzad48@gmail.com>
2026-02-01 19:37:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,23 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const { v4: uuidV4 } = require('uuid');
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/join', (req, res) => {
-  res.send({ link: uuidV4() });
-});
-
 io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).emit('user-connected', userId);
-  });
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit('user-connected', userId);
+        
+        socket.on('chat-msg', (data) => {
+            io.to(roomId).emit('new-msg', data);
+        });
+
+        socket.on('disconnect', () => {
+            socket.broadcast.to(roomId).emit('user-disconnected', userId);
+        });
+    });
 });
 
-server.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`PHOENIX GLOBAL NODE ACTIVE ON ${PORT}`));
